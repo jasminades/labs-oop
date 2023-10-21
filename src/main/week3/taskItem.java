65525305550056e9203bae702c9173618a311c4c
Your Name <you@example.com>
2023-10-21 16:25:45 +0200

    initial commit with week3 project

---
+++ src/main/week3/taskItem.java
@@ -0,0 +1,17 @@
+public enum Status {
+    TO_DO, IN_PROGRESS, COMPLETED, CANCELLED
+}
+
+public class TaskItem {
+    private int taskId;
+    private String taskDescription;
+    private Status taskStatus;
+
+    public TaskItem(int taskId, String taskDescription, Status taskStatus) {
+        this.taskId = taskId;
+        this.taskDescription = taskDescription;
+        this.taskStatus = taskStatus;
+    }
+
+    // Add getter methods and any other necessary methods
+}
