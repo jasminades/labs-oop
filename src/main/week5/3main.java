c68fdbe3f58ba7fd3bafd563d6036d1a32543661
Jasmina <123311005+jasminades@users.noreply.github.com>
2023-11-02 14:00:49 +0100

    Update 3main.java

--- src/main/week5/3main.java
+++ src/main/week5/3main.java
@@ -2,26 +2,26 @@ package boxes;
 
 public class Main {
     public static void main(String[] args) {
-        // Upcasting
+       
         Box genericBox = new Box();
         Box maxWeightBox = new MaxWeightBox(10);
         Box oneThingBox = new OneThingBox();
         Box blackHoleBox = new BlackHoleBox();
 
-        // Downcasting with instanceof
+       
         if (genericBox instanceof MaxWeightBox) {
             MaxWeightBox castedMaxWeightBox = (MaxWeightBox) genericBox;
-            // Use castedMaxWeightBox...
+         
         }
 
         if (genericBox instanceof OneThingBox) {
             OneThingBox castedOneThingBox = (OneThingBox) genericBox;
-            // Use castedOneThingBox...
+          
         }
 
         if (genericBox instanceof BlackHoleBox) {
             BlackHoleBox castedBlackHoleBox = (BlackHoleBox) genericBox;
-            // Use castedBlackHoleBox...
+            
         }
     }
 }
